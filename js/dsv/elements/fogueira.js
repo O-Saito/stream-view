5e6e7ddeac53bf40e91b85be7b6c67d47819ca52
Saito <75994293+O-Saito@users.noreply.github.com>
2025-04-09 18:24:54 +0000

    Added light source

--- js/dsv/elements/fogueira.js
+++ js/dsv/elements/fogueira.js
@@ -1,6 +1,7 @@
 import Prop from './prop.js'
 import game from '../game.js'
 import SimpleDraw from '../components/simpleDraw.js';
+import engine from '../engine.js';
 
 export default class Fogueira extends Prop {
     constructor({ size, position, imageSize, texCoordOffset, texture, depth }) {
@@ -8,6 +9,21 @@ export default class Fogueira extends Prop {
 
         this.isLightSource = true;
         this.components.push(new SimpleDraw(this, this.texture));
+        this.lightId = engine.addLight({
+            pos: { x: this.position.x + Math.ceil(this.objectSize.width / 2), y: this.position.y, z: 1 },
+            color: { r: 0, g: 0, b: 1 }, 
+            intensity: 2.0, 
+            radius: 250.0, 
+            objectId: this.localGlobalId,
+        });
         this.enable();
+    }
+
+    update() {
+        super.update();
+        const  l = engine.getLight(this.lightId);
+        if(!l) return;
+        l.pos.x = this.position.x+ Math.ceil(this.objectSize.width / 2);
+        l.pos.y = this.position.y;
     }
 }
